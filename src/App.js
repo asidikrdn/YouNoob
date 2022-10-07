6f9f9574c72a3594ea6fa3429f3f80ea58fe06ee
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2022-10-07 15:13:19 +0700

    Menambahkan opsi apiKey cadangan apabila apiKey pertama sudah over quota

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
 import { BrowserRouter as Router } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(true);
-  // const [indexApiKey, setIndexApiKey] = useState(0);
   const [listVideos, setListVideos] = useState([]);
   const [listChannels, setListChannels] = useState([]);
+  const [apiKey, setApiKey] = useState(process.env.REACT_APP_FIRST_KEY);
+  const [errStatus, setErrStatus] = useState(false);
 
   const getListVideos = async (q) => {
     try {
@@ -20,13 +21,13 @@ const App = () => {
         {
           method: "GET",
           headers: {
-            // "X-RapidAPI-Key": `${apiKey[indexApiKey]}`,
-            "X-RapidAPI-Key": process.env.REACT_APP_FIRST_KEY,
+            "X-RapidAPI-Key": apiKey,
             "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
           },
         }
       );
       if (!response.ok) {
+        setErrStatus(true);
         throw new Error(
           "Error saat mengambil data dengan kode: " + response.status
         );
@@ -42,6 +43,7 @@ const App = () => {
             return el.type === "channel";
           })
         );
+        setErrStatus(false);
       }
     } catch (e) {
       console.log(e);
@@ -49,6 +51,35 @@ const App = () => {
       setLoading(false);
     }
   };
+
+  useEffect(() => {
+    if (errStatus === true && apiKey === process.env.REACT_APP_FIRST_KEY) {
+      setApiKey(process.env.REACT_APP_SECOND_KEY);
+    } else if (
+      errStatus === true &&
+      apiKey === process.env.REACT_APP_SECOND_KEY
+    ) {
+      setApiKey(process.env.REACT_APP_THIRD_KEY);
+    } else if (
+      errStatus === true &&
+      apiKey === process.env.REACT_APP_THIRD_KEY
+    ) {
+      setApiKey(process.env.REACT_APP_FOURTH_KEY);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [errStatus]);
+
+  useEffect(() => {
+    if (errStatus === true) {
+      query === ""
+        ? getListVideos("berita terbaru indonesia")
+        : getListVideos(query);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [apiKey]);
+
+  // console.log(apiKey);
+  // console.log(query);
 
   return (
     <Router>
